a65cdf2988882d0da344e6460c78703ae42c0fc1
Augusto QM <augustoqmacedo@gmail.com>
2013-02-25 01:23:53 -0300

    OK

--- src/javaCode/collection/util/ApiKeysManager.java
+++ src/javaCode/collection/util/ApiKeysManager.java
@@ -2,19 +2,18 @@ package javaCode.collection.util;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.URLConnection;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
+import javaCode.collection.MainCollection;
 
 public class ApiKeysManager {
 
-	private static final int MIN_REMAINING_CALLS = 5;
-	private static final int MAX_REMAINING_CALLS = 200;
-
-	private static final int MIN_CALL_INTERVAL = 400;
+	private static final int MIN_CALL_INTERVAL = 100;
 	private static final int MAX_CALL_INTERVAL = 4000;
 	private static final int CALL_INTERVAL_CHANGE_TIME_IN_MINUTES = 10;
 	private static final int CALL_INTERVAL_DECREASE_STEP_IN_MILLIS = 100;
@@ -26,7 +25,7 @@ public class ApiKeysManager {
 	private static ArrayList<ApiKey> keys;
 	private static int currentKeyIndex;
 
-	private static Calendar nextHour;
+	// private static Calendar nextHour;
 	private static Calendar lastCallIntervalChange;
 
 	public static void setKeys(String[] newKeys, String[] keyNames) {
@@ -38,12 +37,9 @@ public class ApiKeysManager {
 			}
 			currentKeyIndex = -1;
 
-			nextHour = Calendar.getInstance();
-			updateNextHour();
-
 			lastCallIntervalChange = Calendar.getInstance();
 
-			callIntervalInMillis = 1500;
+			callIntervalInMillis = 500;
 
 			throttledTimes = 0;
 		} else {
@@ -53,18 +49,31 @@ public class ApiKeysManager {
 
 	public static String getKey() {
 		stopForAWhile();
-
-		checkAndResetCallLimits();
-
-		if (!changeApiKey()) {
-			stopExecutionUntilNextHour();
-		}
-
-		// And, to finish, decrement the remaining calls of the new key,
-		// returning it
-		keys.get(currentKeyIndex).decrementCalls();
+		changeApiKey();
 
 		return keys.get(currentKeyIndex).getKey();
+	}
+
+	private static void stopForAWhile() {
+		try {
+			Thread.sleep(callIntervalInMillis);
+		} catch (InterruptedException e) {
+			System.out
+					.println(">>> ATTENTION! The timer was interrupted before the expected time!");
+			e.printStackTrace();
+		}
+	}
+
+	private static void changeApiKey() {
+
+		currentKeyIndex = (currentKeyIndex + 1) % keys.size();
+		System.out.print("            " + keys.get(currentKeyIndex).getName());
+		if (currentKeyIndex == 0) {
+			Date now = new Date(System.currentTimeMillis());
+			SimpleDateFormat f = new SimpleDateFormat("HH:mm:ss 'at' dd.MM.yy");
+			System.out.print("(" + f.format(now) + ")");
+		}
+		System.out.print(": ");
 	}
 
 	public static boolean checkConnectionCondition(URLConnection urlConn)
@@ -125,6 +134,7 @@ public class ApiKeysManager {
 								: callIntervalInMillis;
 
 						System.out.println();
+						System.out.println();
 						System.out.println(">>> Nice! No throttling in "
 								+ CALL_INTERVAL_CHANGE_TIME_IN_MINUTES
 								+ " minutes.");
@@ -142,113 +152,13 @@ public class ApiKeysManager {
 		return result;
 	}
 
-	private static void stopForAWhile() {
-		try {
-			Thread.sleep(callIntervalInMillis);
-		} catch (InterruptedException e) {
-			System.out
-					.println(">>> ATTENTION! The timer was interrupted before the expected time!");
-			e.printStackTrace();
-		}
-	}
-
-	private static void checkAndResetCallLimits() {
-		if (new Date(System.currentTimeMillis()).compareTo(nextHour.getTime()) >= 0) {
-			for (ApiKey k : keys) {
-				k.setRemainingCalls(MAX_REMAINING_CALLS);
-			}
-
-			updateNextHour();
-		}
-	}
-
-	private static boolean changeApiKey() {
-
-		int tmpIndex;
-		boolean result = false;
-		for (int i = 1; i < keys.size(); i++) {
-			tmpIndex = (currentKeyIndex + i) % keys.size();
-
-			if (keys.get(tmpIndex).getRemainingCalls() > MIN_REMAINING_CALLS) {
-				currentKeyIndex = tmpIndex;
-				result = true;
-
-				System.out.print("            "
-						+ keys.get(currentKeyIndex).getName());
-				if (currentKeyIndex == 0) {
-					Date now = new Date(System.currentTimeMillis());
-					SimpleDateFormat f = new SimpleDateFormat(
-							"HH:mm:ss 'at' dd.MM.yy");
-					System.out.print("("
-							+ keys.get(currentKeyIndex).getRemainingCalls()
-							+ " calls - " + f.format(now) + ")");
-				}
-				System.out.print(": ");
-
-				break;
-			} else {
-				System.out.println(">>>  " + keys.get(tmpIndex).getName()
-						+ " key: Call Limit Exceeded");
-			}
-		}
-		return result;
-	}
-
-	private static void stopExecutionUntilNextHour() {
-
-		long millisToWait = nextHour.getTimeInMillis()
-				- System.currentTimeMillis();
-
-		System.out.println();
-		System.out.println(">>>  Script stopped during: "
-				+ String.format(
-						"%d min, %d sec",
-						TimeUnit.MILLISECONDS.toMinutes(millisToWait),
-						TimeUnit.MILLISECONDS.toSeconds(millisToWait)
-								- TimeUnit.MINUTES
-										.toSeconds(TimeUnit.MILLISECONDS
-												.toMinutes(millisToWait))));
-		System.out.println(">>>  Script stopped until: " + nextHour.getTime());
-		try {
-			Thread.sleep(millisToWait + 1);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		System.out.println(">>>  We're back at: "
-				+ new Date(System.currentTimeMillis()));
-		System.out.println();
-	}
-
-	private static void updateNextHour() {
-		Calendar calendar = Calendar.getInstance();
-		calendar.add(Calendar.HOUR, 1);
-		calendar.set(Calendar.MINUTE, 0);
-		calendar.set(Calendar.SECOND, 0);
-		calendar.set(Calendar.MILLISECOND, 0);
-		nextHour = calendar;
-	}
-
 	static class ApiKey {
 		private String key;
 		private String name;
-		private int remainingCalls;
 
 		public ApiKey(String key, String name) {
 			this.key = key;
 			this.name = name;
-			this.remainingCalls = 200;
-		}
-
-		public void decrementCalls() {
-			remainingCalls--;
-		}
-
-		public int getRemainingCalls() {
-			return remainingCalls;
-		}
-
-		public void setRemainingCalls(int remainingCalls) {
-			this.remainingCalls = remainingCalls;
 		}
 
 		public String getKey() {
@@ -261,38 +171,37 @@ public class ApiKeysManager {
 
 		@Override
 		public String toString() {
-			return "Key Name: " + getName() + "\nRemainingCalls: "
-					+ getRemainingCalls();
+			return "Key Name: " + getName();
 		}
 
 	}
 
-	// public static void main(String[] args) throws MalformedURLException,
-	// IOException {
-	// MainCollection.readPropertiesFile();
-	//
-	// // URLConnection urlConn = new URL(URLManager.getEventsURLByMember(
-	// // "4c5f7a107b7624226a67794025897c", new Long(10341972), 0))
-	// // .openConnection();
-	//
-	// URLConnection urlConn = new URL(
-	// "https://api.meetup.com/ew/events?key=7f422a3a6e6d253c7e62585b722a6&sign=true&page=20")
-	// .openConnection();
-	//
-	// System.out.println(urlConn.getHeaderFields());
-	// System.out.println(((HttpURLConnection) urlConn).getResponseCode());
-	//
-	// // BufferedReader br = new BufferedReader(new
-	// // InputStreamReader(urlConn.getInputStream()));
-	// // System.out.println(br.readLine());
-	//
-	// for (int i = 0; i < 2000; i++) {
-	// getKey();
-	// System.out.println();
-	// if (!checkConnectionCondition(urlConn))
-	// continue;
-	//
-	// System.out.println();
-	// }
-	// }
+//	public static void main(String[] args) throws MalformedURLException,
+//			IOException {
+//		MainCollection.readPropertiesFile();
+//
+//		 URLConnection urlConn = new URL(URLManager.getEventsURLByMember(
+//		 "4c5f7a107b7624226a67794025897c", new Long(10341972), 0))
+//		 .openConnection();
+//
+////		URLConnection urlConn = new URL(
+////				"https://api.meetup.com/ew/events?key=7f422a3a6e6d253c7e62585b722a6&sign=true&page=20")
+////				.openConnection();
+//
+//		System.out.println(urlConn.getHeaderFields());
+//		System.out.println(((HttpURLConnection) urlConn).getResponseCode());
+//
+//		// BufferedReader br = new BufferedReader(new
+//		// InputStreamReader(urlConn.getInputStream()));
+//		// System.out.println(br.readLine());
+//
+//		for (int i = 0; i < 2000; i++) {
+//			getKey();
+//			System.out.println();
+//			if (!checkConnectionCondition(urlConn))
+//				continue;
+//
+//			System.out.println();
+//		}
+//	}
 }
