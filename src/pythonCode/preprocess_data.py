9523c4360ffd7d6039e5106d0c9986770419091f
Guilherme Santos <guisgb13@gmail.com>
2013-03-08 02:38:43 -0300

    Preprocessor almost finished

--- src/pythonCode/preprocess_data.py
+++ src/pythonCode/preprocess_data.py
@@ -1,7 +1,9 @@
-import sys
+import sys, re, json
 
 
 DATA_PATH = "../../data/"
+
+OUTPUT = "../../"
 
 if sys.argv[1] == "events":
 	
@@ -9,13 +11,41 @@ if sys.argv[1] == "events":
 
 	i = 1
 
+	lats = []
+	longs = []
+
 	while True:
 		try:
-			f = open(path+"/events_"+str(i)+".json")
+			f = open(path+"/events_"+str(i)+".json", 'r')
 			i += 1
+
+			js = f.read(-1)
+			print "JSON before: ", js, js.count("["), js.count("]")
+			js = re.sub("/\]\s*\[/gm", ",", js)
+			print "JSON later:",  js, js.count("["), js.count("]")
+
+			f.close()
+
+			events = json.loads(js)
+			for event in events:
+				if event['venue'] != None:
+					print "that's true"
+					lats.append(event['venue']['lat'])
+					longs.append(event['venue']['lon'])
 
 		except:
 			break
+
+	if len(lats) <= 0:
+		print "Wasn't useful... =("
+		exit(0)
+
+	o = open("events_data", 'w')
+	o.write("[{'lat' : "+lats[0]+", 'lon' : "+longs[0]+"}")
+	for i in range(1,len(lats)):
+		o.write(",{'lat' : "+lats[i]+", 'lon' : "+longs[i]+"}")
+	o.write("]")
+	o.close()
 
 else:
 	print "Nothing to do"
