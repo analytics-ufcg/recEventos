d831a406f60301f55985da9bef61744e87b4762f
Guilherme Santos <guisgb13@gmail.com>
2013-03-15 08:29:51 -0300

    Server now reads users from csv files

--- src/web/app.py
+++ src/web/app.py
@@ -18,7 +18,6 @@ def index():
 		render_template("index.html", venues=[], users=[])
 
 	venues = []
-	users = []
 	try:
 		first = True
 		for venue in f.readlines():
@@ -29,7 +28,7 @@ def index():
 			venue = venue.split(",")
 
 
-			#venue[0] - id; venue[1] - longitude; venue[2] - latitude
+			#venue[0] - id; venue[1] - longitude; venue[2] - latitude; venue[3] - name
 			if venue[1].strip() == "" or venue[2].strip() == "":
 				continue
 			if venue[1].isspace() or venue[2].isspace():
@@ -38,9 +37,35 @@ def index():
 				continue
 			venues.append( { 'id' : venue[0], 'name' : filter(lambda x: x in string.printable, venue[3]), 'lon' : venue[1], 'lat' : venue[2] } )			
 	except:
-		print "Error parsing CSV occured"
+		print "Error parsing venues CSV occured"
 	finally:
 		f.close()
+
+	users = []
+	try:
+		for i in range(1,6):
+			f = open(os.path.join("data_output", "members_"+str(i)+".csv"), 'r')
+			
+			first = True
+			for user in f.readlines():
+				if first:
+					first = False
+					continue
+				user = user.replace("\"", "")
+				user = user.split(",")
+
+
+				#user[0] - id; user[1] - name; user[4] - longitude; user[5] - latitude
+				if user[4].strip() == "" or user[5].strip() == "":
+					continue
+				if user[4].isspace() or user[5].isspace():
+					continue
+				elif user[4].isalpha() or user[5].isalpha():
+					continue
+				users.append( { 'id' : user[0], 'name' : filter(lambda x: x in string.printable, user[1]), 'lon' : user[4], 'lat' : user[5] } )
+	except IOError:
+		pass
+
 	return render_template("index.html", venues=venues, users=users)
 
 @app.route('/venue_events/<venue_ids>', methods=['GET'])
