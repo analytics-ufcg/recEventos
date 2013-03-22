4003a4cd57c5963abce43d33df6a782307575594
Augusto QM <augustoqmacedo@gmail.com>
2013-03-22 12:31:00 -0300

    New data_summary with member-event distance analysis New view_prepare_tables with the tables optimally organized to be used by the view

--- src/rCode/sprint_4/view_prepare_tables.R
+++ src/rCode/sprint_4/view_prepare_tables.R
@@ -29,16 +29,6 @@ members <- members[members$id %in% unique(member.events$member_id),]
 members <- members[members$city %in% unique(venues$venue_city),]
 colnames(members) <- c("member_id", "member_lat", "member_lon", "member_name", "member_city")
 
-# Merge member.events, events and venues 
-print(noquote("Merge the member.events with events data "))
-member.events.venue <- merge(member.events, events, by = "event_id")
-
-print(noquote("Merge the member.events + events data with venues data"))
-member.events.venue <- merge(member.events.venue, venues, by = "venue_id")
-member.events.venue <- member.events.venue[,c("member_id", "event_id", "event_name", 
-                                              "event_time", "venue_id", "venue_lat", 
-                                              "venue_lon", "venue_name", "venue_city")]
-
 # Persist in data_view
 print(noquote("Persist the data"))
 dir.create("data_output/view/", showWarnings=F)
@@ -46,4 +36,3 @@ write.csv(members,"data_output/view/members.csv", row.names = F)
 write.csv(events,"data_output/view/events.csv", row.names = F)
 write.csv(venues,"data_output/view/venues.csv", row.names = F)
 write.csv(member.events,"data_output/view/member_events.csv", row.names = F)
-write.csv(member.events.venue,"data_output/view/member_events_venue.csv", row.names = F)
